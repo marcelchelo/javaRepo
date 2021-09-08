10ea6616b19ad571ddccf4af3fac1d7831eb9241
Marcelo <marcelo95326@icloud.com>
2021-09-07 22:51:20 -0400

    added comments tests for letter m in array to prove functionality.

--- isUnique/isUnique/src/isUnique/isUnique.java
+++ isUnique/isUnique/src/isUnique/isUnique.java
@@ -7,6 +7,8 @@ import java.util.Map;
 //example:    "Hello"    Not unique 
 //			"name"  is Unique
 
+//Marcelo Villalba
+
 public class isUnique {
 public static void main (String [] args) {
 	
@@ -35,11 +37,17 @@ private static boolean isUnique(String word) {
 	boolean [] char_set = new boolean[128]; //array of fixed size 128
 	for (int i=0 ; i<word.length(); i++) {//traverse the  given word.
 		int val = word.charAt(i);  //obtain ascii value at index I
-		System.out.println(val); //print the value to screen   (not needed)
-		if(char_set[val] == true) {   //we are essentially putting char a in index 97(its corresponding ascii value) and checking if it exists there. If it does return false bc it is a repeat.
+		//System.out.println(val); //print the value to screen   (not needed)
+		if(char_set[val] ) { //we are essentially putting char a in index 97(its corresponding ascii value) and checking if it exists there. If it does return false bc it is a repeat.
+			System.out.println("Repeated letter is: " + char_set[i]);
 			return false;
 		}
-		char_set[val] = true;     
+		//Test, Print the index location of letter m
+		
+		System.out.println(char_set[109]);  //Test Location 109 is for letter m.  it will return true  if word is marcelo
+		char_set[val] = true;    
+		
+		
 	}
 	
 	
